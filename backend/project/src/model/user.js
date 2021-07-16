5d731ece65285d0e54f1346f0466cb162476c210
ebrahim salehi <eeeee.bbbbb.iiiii@gmail.com>
2021-07-16 22:13:09 +0430

    add backend to project - prisma , ...

---
+++ backend/project/src/model/user.js
@@ -0,0 +1,24 @@
+//import {PrismaClient} from '@prisma/client';
+
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient()
+
+const fetchAll = async ()=>{
+    const users = await prisma.users.findMany();
+    console.log(users);
+}
+
+const save = async (objUser) =>{
+    const result =  await prisma.users.create({
+        data:objUser
+    }).then(res=>{
+        return res;
+    }).catch(err=>{
+        return err;
+    });
+
+}
+
+//fetchAll();
+const r = save({firstName:'zahra',lastName:'omidi',email:'zahra-omidi@gmail.com',avatar:''});
+console.log(r);
