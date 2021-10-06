3fa030c3b4a5dea8e064b77cd4244bd33c2de3e6
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2021-10-06 17:25:05 +0330

    add some components

--- src/Components/Manoyar/Security/Login.js
+++ src/Components/Manoyar/Security/Login.js
@@ -2,23 +2,59 @@ import React from 'react';
 import Grid from '../BaseComponents/Grid';
 import TextField from '../BaseComponents/TextField';
 import Button from '../BaseComponents/Button';
+import {useFormik} from 'formik';
+import * as yup from 'yup';
+
+const validationSchema=yup.object({
+    userName:yup.string().required('user name must be entered'),
+    password:yup.string().required('password must be entered')
+});
 
 export default function Login(){
+
+    const formik = useFormik({
+        initialValues:{
+            userName:'',
+            password:''
+        },
+        onSubmit:values=>{
+            console.log(values);
+        },
+        validationSchema:validationSchema
+    });
+
+    console.log('Login is rendering ...',formik);
+
     return (
+    <form onSubmit={formik.handleSubmit}>
     <Grid container spacing={2}>
+       
         <Grid item xs={12} md={12}>
             <TextField
+            name="userName"
+            id="userName"
             label="نام کاربری"
+            value={formik.values.userName}
+            onChange={formik.handleChange}
+            error={formik.touched.userName && Boolean(formik.errors.userName)}
+            helperText={formik.touched.userName && formik.errors.userName}
             />
         </Grid>
         <Grid item xs={12} md={12}>
             <TextField
+            name="password"
+            id="password"
             label={'رمزورود'}   
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
             />
         </Grid>
         <Grid item xs={12} md={12}>
-            <Button>ورود به سیستم</Button>
-        </Grid>        
+            <Button type="submit">ورود به سیستم</Button>
+        </Grid>    
+        
     </Grid>
+    </form>
     )
 }
